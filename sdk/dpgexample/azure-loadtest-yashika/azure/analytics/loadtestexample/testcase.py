7f71aa7145d1f3219e8385014abe28a2f0fde5e6
Yashika Jain <t-yjain@microsoft.com>
2022-05-29 01:06:07 +0530

    testcase added

---
+++ sdk/dpgexample/azure-loadtest-yashika/azure/analytics/loadtestexample/testcase.py
@@ -0,0 +1,9 @@
+import sys
+sys.path.append('D:\Project\azure-sdk-for-python\sdk\dpgexample\azure-loadtest-yashika\azure\analytics\loadtestexample\_client.py')
+
+
+import _client
+from _client import LoadTestClient
+
+
+LoadTestClient.__init__()
